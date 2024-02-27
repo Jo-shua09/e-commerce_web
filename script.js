6702552cf67357f610166b85f6691cbc50b1257e
jo-shua33 <126611915+jo-shua33@users.noreply.github.com>
2024-02-27 04:26:45 -0800

    tenth commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function loader() {
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+function fadeOut() {
+  setInterval(loader, 4000);
+}
+window.onload = fadeOut;
+
 // navbar settings
 const navBar = document.getElementById("navbar");
 const menuButton = document.getElementById("menu");
@@ -6,12 +14,174 @@ menuButton.addEventListener("click", () => {
   navBar.classList.toggle("show");
   menuButton.classList.toggle("fa-times");
 });
+
+// close and open cart
+const cartOpen = document.getElementById("cart-icon");
+const cartItems = document.querySelector("#cart-items");
+const cartClose = document.querySelector("#close-cart");
+const nav = document.getElementById("navbar");
+const cartRemove_btns = document.querySelectorAll(".remove-items");
+const addCart_btns = document.querySelectorAll("#addbtn");
+
+cartOpen.onclick = function () {
+  cartItems.classList.add("showCart");
+  nav.classList.add("remove");
+};
+cartClose.onclick = function () {
+  cartItems.classList.remove("showCart");
+  nav.classList.remove("remove");
+};
 window.onscroll = function () {
   navBar.classList.remove("show");
   menuButton.classList.remove("fa-times");
 };
 
-// swiper settings
+// starts when the document is ready
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", start);
+} else {
+  start();
+}
+
+// =========== start ==============
+function start() {
+  addEvents();
+}
+
+// =========== update ==============
+function update() {
+  addEvents();
+  updateTotals();
+}
+
+// =========== addEvents ==============
+function addEvents() {
+  cartRemove_btns.forEach((btn) => {
+    btn.addEventListener("click", handle_removerCartItem);
+  });
+
+  // =========== change item quantity ==============
+  let cartQuantity_inputs = document.querySelectorAll(".cart-quantity");
+  cartQuantity_inputs.forEach((input) => {
+    input.addEventListener("change", handle_changeItemQuantity);
+  });
+
+  // ============== add items to cart =================
+  // addCart_btns.addEventListener("click", handle_addCartItem);
+  addCart_btns.forEach((btn) => {
+    btn.addEventListener("click", handle_addCartItem);
+  });
+
+  // check out
+  const checkOut_btn = document.querySelector(".checkOut");
+  checkOut_btn.addEventListener("click", handle_checkOut);
+}
+
+// ============= HANDLE EVENTS FUNCTIONS =============
+let itemsAdded = [];
+function handle_addCartItem() {
+  product = document.querySelector(" div.box-content");
+  let title = product.querySelector(".product-title").innerHTML;
+  let price = product.querySelector("#prices").innerHTML;
+  let imgSrc = product.querySelector(".products-img").src;
+  console.log(title, price, imgSrc);
+
+  let newToAdd = {
+    title,
+    price,
+    imgSrc,
+  };
+
+  // handle item is already exist
+  if (itemsAdded.find((el) => el.title == newToAdd.title)) {
+    alert("Product Already In Cart!");
+    return;
+  } else {
+    itemsAdded.push(newToAdd);
+  }
+
+  let cartBoxElement = cartBoxComponent(title, price, imgSrc);
+  let newNode = document.createElement("div");
+  newNode.innerHTML = cartBoxElement;
+  const cartContent = document.querySelector(".cart-content");
+  cartContent.appendChild(newNode);
+
+  update();
+}
+
+function handle_removerCartItem() {
+  this.parentElement.remove();
+  itemsAdded = itemsAdded.filter(
+    (el) =>
+      el.title !=
+      this.parentElement.querySelector(".cart-product-title").innerHTML
+  );
+
+  update();
+}
+
+function handle_changeItemQuantity() {
+  if (isNaN(this.value) || this.value < 1) {
+    this.value = 1;
+  }
+  this.value = Math.floor(this.value); //to keep it in an integer value
+
+  update();
+}
+
+function handle_checkOut() {
+  if (itemsAdded.length <= 0) {
+    alert("Your Cart Is Empty! 'add products to cart'");
+  }
+  const cartContent = document.querySelector(".cart-content");
+  cartContent.innerHTML = "";
+  alert("Successfully Check Out Product(s)");
+  itemsAdded = [];
+  update();
+}
+
+function updateTotals() {
+  let cartBoxes = document.querySelectorAll(".cart-box");
+  const totalElement = document.querySelector(".totalA");
+  const totalElementB = document.querySelector(".totalB");
+  let total = 0;
+  cartBoxes.forEach((cartBox) => {
+    let priceElement = cartBox.querySelector(".cart-price");
+    let price = parseFloat(priceElement.innerHTML.replace("$", ""));
+    let quantity = cartBox.querySelector(".cart-quantity").value;
+
+    total += price * quantity;
+  });
+
+  total = total.toFixed(2); // to made it a decimal of 2 numbers
+
+  totalElement.innerHTML = "$" + total;
+  totalElementB.innerHTML = "$" + total;
+}
+
+function cartBoxComponent(title, price, imgSrc) {
+  return `
+  <div class="cart-box">
+   <img src=${imgSrc} alt="" class="cart-img" />
+   <div class="detail-box">
+     <div class="cart-product-title">${title}</div>
+     <div class="cart-price">${price}</div>
+     <div class="cart-size">
+       <select name="size" id="select">
+         <option value="small">S</option>
+         <option value="medium">M</option>
+         <option value="large">L</option>
+         <option value="extra large">XL</option>
+         <option value="extrra extra">XXL</option>
+       </select>
+     </div>
+     <input type="number" value="1" class="cart-quantity" />
+   </div>
+   <i class="fas fa-trash remove-items"></i>
+ </div>`;
+}
+
+//swiper settings
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   centeredSlides: true,
@@ -41,7 +211,7 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-// image changer
+//image changer
 const mainImg = document.querySelector("#mainImg");
 const subImg = document.getElementsByClassName("smallImg");
 
@@ -74,23 +244,4 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-// const backWard = document.getElementById("back");
-// const forWard = document.getElementById("forward");
-// const firstPage = document.getElementById("first-blog");
-// const secondPage = document.getElementById("second-blog");
-
-// forWard.onclick = function () {
-//   firstPage.classList.toggle("remove");
-//   secondPage.classList.toggle("show");
-// };
-// backWard.onclick = function () {
-//   firstPage.classList.remove("remove");
-//   secondPage.classList.remove("show");
-// };
-// forWard.addEventListener("click", () => {
-//   firstPage.classList.toggle("remove");
-//   secondPage.classList.toggle("show");
-// });
-// secondPage.scroll = () => {
-//   secondPage.classList.toggle("show");
-// };
+// cart open and close
